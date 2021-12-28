02c8a23c7d1d99b50c34d1379ab8b57dc43facf9
Andrei Sogomonian <saipeg@yandex.ru>
2021-12-28 22:24:33 +0300

    git init

---
+++ src/main/java/com/sogomonian/spring/security/spring_security/JavaTelegramQuizApplication.java
@@ -0,0 +1,13 @@
+package com.sogomonian.spring.security.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelegramQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTelegramQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sogomonian/spring/security/spring_security/JavaTelegrammQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogomonian.spring.security.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTelegrammQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
