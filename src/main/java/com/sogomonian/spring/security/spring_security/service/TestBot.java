d860612c58c54c358b9465a39ff366962c912d74
Andrei Sogomonian <saipeg@yandex.ru>
2021-12-31 23:55:44 +0300

    Change extends to TelegramLongPollingBot

--- src/main/java/com/sogomonian/spring/security/spring_security/service/TestBot.java
+++ src/main/java/com/sogomonian/spring/security/spring_security/service/TestBot.java
@@ -1,11 +1,37 @@
 package com.sogomonian.spring.security.spring_security.service;
 
 import lombok.SneakyThrows;
-import org.telegram.telegrambots.bots.DefaultAbsSender;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class TestBot extends DefaultAbsSender {
+public class TestBot extends TelegramLongPollingBot {
+
+    @Override
+    @SneakyThrows
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                execute(
+                        SendMessage.builder()
+                                .chatId(message.getChatId().toString())
+                                .text("You sent: \n\n" + message.getText())
+                                .build());
+            }
+
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "@omg_commerce_omg_bot";
+    }
+
     @Override
     public String getBotToken() {
         return "";
@@ -13,7 +39,10 @@ public class TestBot extends DefaultAbsSender {
 
     @SneakyThrows
     public static void main(String[] args) {
-        TestBot testBot = new TestBot(new DefaultBotOptions());
-        testBot.execute(SendMessage.builder().chatId("").text("Hello From Java").build());
+        TestBot bot = new TestBot();
+//        bot.execute(SendMessage.builder().chatId("279116").text("Hello From Java").build());
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
     }
+
 }
